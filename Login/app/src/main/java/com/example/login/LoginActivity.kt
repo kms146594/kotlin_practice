aac88f274d5815f1f6b7a4c6d383021368c3b4bc
kms146594 <kms146594@gmail.com>
2024-04-04 13:31:45 +0900

    back_color

--- Login/app/src/main/java/com/example/login/LoginActivity.kt
+++ Login/app/src/main/java/com/example/login/LoginActivity.kt
@@ -2,8 +2,10 @@ package com.example.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.login.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
